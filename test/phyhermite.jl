da6d28e33171ce94521f01ee0fc8223409b66254
mleprovost <mathow@g.ucla.edu>
2020-07-06 17:54:08 -0700

    Need to add test for integration and rescaled functions

--- test/phyhermite.jl
+++ test/phyhermite.jl
@@ -2,38 +2,51 @@
 
 @testset "Verify evaluation of Physicist hermite functions" begin
 
+    # @testset "Verify evaluation of Physicist hermite functions" begin
+    x = randn(100)
+    m = 5
+    dV = vander(PhyHermite(m), 0, x; scaled = false)
 
-x = randn(10)
-m = 5
-dV = vander(PhyHermite(m), 0, x; scaled = false)
+    for i=0:5
+        @test norm(dV[:,i+1] - FamilyPhyPolyHermite[i+1].(x) .* exp.(-x.^2/2))<1e-8
+    end
 
-for i=0:5
-    @test dV[:,i+1] == PhyHermite[i+1].(x).*exp.(-x.^2/2)
+    dV = vander(PhyHermite(m), 0, x; scaled = true)
+
+
+    for i=0:5
+        @test dV[:,i+1] == FamilyScaledPhyPolyHermite[i+1].(x).*exp.(-x.^2/2)
+    end
 end
 
-dV = vander(PhyHermite(m), 0, x; scaled = true)
+@testset "Verify 1st derivative of Physicist hermite functions" begin
 
+    x = randn(100)
+    m = 5
+    dV = vander(PhyHermite(m), 1, x; scaled = false)
 
-for i=0:5
-    @test dV[:,i+1] == PhyHermite[i+1].(x).*exp.(-x.^2/2)
-end
-
-
-end
-
-@testset "Verify derivative of Physicist hermite functions" begin
-
-
-
-
-
+    for i=0:5
+        @test norm(dV[:,i+1] - (FamilyPhyPolyHermite[i+1].(x) .* (-x) .* exp.(-x.^2/2) +
+            derivative(FamilyPhyPolyHermite[i+1],1).(x) .* exp.(-x.^2/2)))<1e-8
+    end
 end
 
 
 @testset "Verify second derivative of Physicist hermite functions" begin
 
+    x = randn(100)
+    m = 5
+    dV = vander(PhyHermite(m), 2, x; scaled = false)
 
+    for i=0:5
+        Hn = FamilyPhyPolyHermite[i+1].(x)
+        Hnp = derivative(FamilyPhyPolyHermite[i+1],1).(x)
+        Hnpp = derivative(FamilyPhyPolyHermite[i+1],2).(x)
+        E = exp.(-x.^2/2)
 
-
-
+        @test norm(dV[:,i+1] - (Hnpp .*E -2 .*x .* Hnp.*E + Hn .* (x.^2 .- 1.0) .* E))<1e-8
+    end
 end
+
+
+# Need to write test for integration
--- test/prohermite.jl
+++ test/prohermite.jl
@@ -1,1 +1,48 @@
 
+@testset "Verify evaluation of Probabilistic hermite functions" begin
+
+    # @testset "Verify evaluation of Physicist hermite functions" begin
+    x = randn(100)
+    m = 5
+    dV = vander(ProHermite(m), 0, x; scaled = false)
+
+    for i=0:5
+        @test norm(dV[:,i+1] - FamilyProPolyHermite[i+1].(x) .* exp.(-x.^2/4))<1e-8
+    end
+
+    dV = vander(ProHermite(m), 0, x; scaled = true)
+
+
+    for i=0:5
+        @test dV[:,i+1] == FamilyScaledProPolyHermite[i+1].(x).*exp.(-x.^2/4)
+    end
+end
+
+@testset "Verify 1st derivative of Probabilistic hermite functions" begin
+
+    x = randn(100)
+    m = 5
+    dV = vander(ProHermite(m), 1, x; scaled = false)
+
+    for i=0:5
+        @test norm(dV[:,i+1] - (FamilyProPolyHermite[i+1].(x) .* (-0.5*x) .* exp.(-x.^2/4) +
+            derivative(FamilyProPolyHermite[i+1],1).(x) .* exp.(-x.^2/4)))<1e-8
+    end
+end
+
+
+@testset "Verify second derivative of Probabilistic hermite functions" begin
+
+    x = randn(100)
+    m = 5
+    dV = vander(ProHermite(m), 2, x; scaled = false)
+
+    for i=0:5
+        Hen = FamilyProPolyHermite[i+1].(x)
+        Henp = derivative(FamilyProPolyHermite[i+1],1).(x)
+        Henpp = derivative(FamilyProPolyHermite[i+1],2).(x)
+        E = exp.(-x.^2/4)
+
+        @test norm(dV[:,i+1] - (Henpp .*E - x .* Henp.*E + 0.5*Hen .* (0.5*x.^2 .- 1.0) .* E))<1e-8
+    end
+end
