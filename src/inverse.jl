ad7c10c6f284b30c614a417790a15530d15b620a
mleprovost <mathow@g.ucla.edu>
2020-07-22 10:46:19 -0700

    Improve tools for inversion

--- src/inverse.jl
+++ src/inverse.jl
@@ -1,6 +1,39 @@
-export functional!
+export functionalf!, functionalg!, functionalfg!
 
-function functional!(F, J, xk, cache, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx}
+function functionalf!(F, xk, cacheF, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx}
+    function integrand!(v::Vector{Float64}, t::Float64)
+        # repeated_grad_xk_basis(cacheF, R.f.f,  t*xk)
+        cacheF .= repeated_grad_xk_basis(R.f.f,  t*xk)
+        @avx @. v = (cacheF .* ψoff) *ˡ R.f.f.coeff
+        evaluate!(v, R.g, v)
+        # v .= R.g((repeated_grad_xk_basis(R.f.f,  t*xk) .* ψoff)*R.f.f.coeff)
+    end
+
+    quadgk!(integrand!, F, 0, 1; rtol = 1e-3)
+    F .*= xk
+    F .-= output
+    nothing
+end
+
+functionalf!(cacheF, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx} =
+    (F, xk) -> functionalf!(F, xk, cacheF, ψoff, output, R)
+
+function functionalg!(J, xk, cache, cacheJ, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx}
+    cache .= repeated_grad_xk_basis(R.f.f,  xk)
+    # epeated_grad_xk_basis!(cache, R.f.f,  xk)
+
+    @avx @. cacheJ = (cache .* ψoff) *ˡ R.f.f.coeff
+    evaluate!(cacheJ, R.g, cacheJ)
+    J .= Diagonal(cacheJ)
+    nothing
+end
+
+
+functionalg!(cache, cacheJ, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx} =
+    (J, xk) -> functionalg!(J, xk, cache, cacheJ, ψoff, output, R)
+
+
+function functionalfg!(F, J, xk, cache, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx}
 
     if !(F == nothing)
         function integrand!(v::Vector{Float64}, t::Float64)
@@ -16,5 +49,5 @@ function functional!(F, J, xk, cache, ψoff, output::Array{Float64,1}, R::Integr
 end
 
 
-functional!(cache, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx} =
-    (F, J, xk) -> functional!(F, J, xk, cache, ψoff, output, R)
+functionalfg!(cache, ψoff, output::Array{Float64,1}, R::IntegratedFunction{m, Nψ, Nx}) where {m, Nψ, Nx} =
+    (F, J, xk) -> functionalfg!(F, J, xk, cache, ψoff, output, R)
--- src/rectifier.jl
+++ src/rectifier.jl
@@ -1,6 +1,9 @@
 
 
 export  Rectifier,
+        square, dsquare, d2square,
+        softplus, dsoftplus, d2softplus, invsoftplus,
+        explinearunit, dexplinearunit, d2explinearunit, invexplinearunit,
         inverse!, inverse,
         grad_x!, grad_x,
         hess_x!, hess_x,
@@ -12,11 +15,22 @@ struct Rectifier
     T::String
 end
 
-# function Rectifier(T)
-#     @assert T ∈ ["squared", "exponential", "softplus", "explinearunit"] "Type of rectifier is not defined"
-#     return Rectifier(T)
-# end
+square(x) = x^2
+dsquare(x) = 2.0*x
+d2square(x) = 2.0
 
+# Softplus tools
+softplus(x) = (log(1.0 + exp(-abs(log(2.0)*x))) + max(log(2.0)*x, 0.0))/log(2.0)
+dsoftplus(x) = 1/(1 + exp(-log(2.0)*x))
+d2softplus(x) = log(2.0)/(2.0*(1.0 + cosh(log(2.0)*x)))
+invsoftplus(x) = min(log(exp(log(2.0)*x) - 1.0)/log(2.0), x)
+
+
+
+explinearunit(x) = x < 0.0 ? exp(x) : x + 1.0
+dexplinearunit(x) = x < 0.0 ? exp(x) : 1.0
+d2explinearunit(x) = x < 0.0 ? exp(x) : 0.0
+invexplinearunit(x) = x < 1.0 ? log(x) : x - 1.0
 
 # Type of the rectifier should be in the following list:
 # "squared", "exponential", "softplus", "explinearunit"
@@ -26,24 +40,33 @@ Rectifier() = Rectifier("softplus")
 
 function (g::Rectifier)(x::T) where {T <: Real}
     if g.T=="squared"
-        return x^2
+        return square(x)
     elseif g.T=="exponential"
         return exp(x)
     elseif g.T=="softplus"
-        a = log(2)
-        return (log(1 + exp(-abs(a*x))) + max(a*x, 0))/a
+        return softplus(x)
     elseif g.T=="explinearunit"
-        if x<0
-            return exp(x)
-        else
-            return x+1.0
-        end
+        return explinearunit(x)
     end
 end
-# @. g(x)#
-(g::Rectifier)(x::Array{T,1}) where {T <: Real} = map!(g, zeros(T, size(x,1)), x)
 
-evaluate!(result::Vector{T}, g::Rectifier, x::Array{T,1}) where {T <: Real} = map!(g, result, x)
+function evaluate!(result::Array{T,1}, g::Rectifier, x::Array{T,1}) where {T <: Real}
+    if g.T=="squared"
+        map!(square, result, x)
+        return result
+    elseif g.T=="exponential"
+        map!(exp, result, x)
+        return result
+    elseif g.T=="softplus"
+        map!(softplus, result, x)
+        return result
+    elseif g.T=="explinearunit"
+        map!(explinearunit, result, x)
+        return result
+    end
+end
+
+(g::Rectifier)(x::Array{T,1}) where {T <: Real} = evaluate!(zero(x), g, x)
 
 
 function inverse(g::Rectifier, x::T) where {T <: Real}
@@ -51,60 +74,90 @@ function inverse(g::Rectifier, x::T) where {T <: Real}
     if g.T=="squared"
         error("squared rectifier is not invertible")
     elseif g.T=="exponential"
-        return exp(x)
+        return log(x)
     elseif g.T=="softplus"
-        a = log(2)
-        return min(log(exp(a*x) - 1)/a, x)
+        return invsoftplus(x)
     elseif g.T=="explinearunit"
-        if x<1
-            return log(x)
-        else
-            return x - 1.0
-        end
+        return invexplinearunit(x)
     end
 end
 
-inverse!(result::Array{T,1}, g::Rectifier, x::Array{T,1}) where {T <: Real} = map!(xi -> inverse(g,xi), result, x)
+function inverse!(result::Array{T,1}, g::Rectifier, x::Array{T,1}) where {T <: Real}
+    @assert all(x .> 0) "Input to rectifier is negative"
+    if g.T=="squared"
+        error("squared rectifier is not invertible")
+    elseif g.T=="exponential"
+        map!(log, result, x)
+        return result
+    elseif g.T=="softplus"
+        map!(invsoftplus, result, x)
+        return result
+    elseif g.T=="explinearunit"
+        map!(invexplinearunit, result, x)
+        return result
+    end
+end
+
 inverse(g::Rectifier, x::Array{T,1}) where {T <: Real} = inverse!(zero(x), g, x)
 
 
 function grad_x(g::Rectifier, x::T) where {T <: Real}
     if g.T=="squared"
-        return 2*x
+        return dsquare(x)
     elseif g.T=="exponential"
         return exp(x)
     elseif g.T=="softplus"
-        a = log(2)
-        return 1/(1 + exp(-a*x))
+        return dsoftplus(x)
     elseif g.T=="explinearunit"
-        if x<0
-            return exp(x)
-        else
-            return 1.0
-        end
+        return dexplinearunit(x)
     end
 end
 
-grad_x!(result::Array{T,1}, g::Rectifier, x::Array{T,1}) where {T <: Real} = map!(xi -> grad_x(g,xi), result, x)
+function grad_x!(result::Array{T,1}, g::Rectifier, x::Array{T,1}) where {T <: Real}
+    if g.T=="squared"
+        map!(dsquare, result, x)
+        return result
+    elseif g.T=="exponential"
+        map!(exp, result, x)
+        return result
+    elseif g.T=="softplus"
+        map!(dsoftplus, result, x)
+        return result
+    elseif g.T=="explinearunit"
+        map!(dexplinearunit, result, x)
+        return result
+    end
+end
+
 grad_x(g::Rectifier, x::Array{T,1}) where {T <: Real} = grad_x!(zero(x), g, x)
 
 
 function hess_x(g::Rectifier, x::T) where {T <: Real}
     if g.T=="squared"
-        return 2.0
+        return d2square(x)
     elseif g.T=="exponential"
         return exp(x)
     elseif g.T=="softplus"
-        a = log(2)
-        return a/(2 + exp(a*x) + exp(-a*x))
+        return d2softplus(x)
     elseif g.T=="explinearunit"
-        if x<0
-            return exp(x)
-        else
-            return 0.0
-        end
+        return d2explinearunit(x)
     end
 end
 
-hess_x!(result::Array{T,1}, g::Rectifier, x::Array{T,1}) where {T <: Real} = map!(xi -> hess_x(g,xi), result, x)
+function hess_x!(result::Array{T,1}, g::Rectifier, x::Array{T,1}) where {T <: Real}
+    if g.T=="squared"
+        map!(d2square, result, x)
+        return result
+    elseif g.T=="exponential"
+        map!(exp, result, x)
+        return result
+    elseif g.T=="softplus"
+        map!(d2softplus, result, x)
+        return result
+    elseif g.T=="explinearunit"
+        map!(d2explinearunit, result, x)
+        return result
+    end
+end
+
 hess_x(g::Rectifier, x::Array{T,1}) where {T <: Real} = hess_x!(zero(x), g, x)
