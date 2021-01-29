ec852c7e1a01d62349a15dc043edc757c50a4a7f
mleprovost <mathow@g.ucla.edu>
2021-01-29 08:34:00 -0800

    Add functions  state and meas

--- src/tools/view.jl
+++ src/tools/view.jl
@@ -1,4 +1,8 @@
-export viewmeas, viewstate
+export meas, viewmeas, state, viewstate
 
-viewmeas(X, Ny, Nx)= view(X, 1:Ny, :)
-viewstate(X, Ny, Nx)= view(X, Ny+1:Ny+Nx, :)
+
+meas(X, Ny, Nx) = X[1:Ny,:]
+viewmeas(X, Ny, Nx) = view(X, 1:Ny,:)
+
+state(X, Ny, Nx) = X[Ny+1:Nx+Ny,:]
+viewstate(X, Ny, Nx) = view(X, Ny+1:Nx+Ny,:)
