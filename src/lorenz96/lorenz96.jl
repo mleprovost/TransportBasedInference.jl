bfe997517bdaeb7f8767dbffc732488f503d8784
mleprovost <mathow@g.ucla.edu>
2021-04-28 11:21:20 -0700

    Lorenz 96 is working with the localized nonlinear ensemble filter

--- src/lorenz96/lorenz96.jl
+++ src/lorenz96/lorenz96.jl
@@ -1,4 +1,4 @@
-export lorenz96!, generate_lorenz96, spin_lorenz96, setup_lorenz96, benchmark_lorenz96
+export lorenz96!, generate_lorenz96, spin_lorenz96, setup_lorenz96, benchmark_lorenz96, setup_order
 
 
 """
@@ -167,6 +167,54 @@ end
 return Metric_list
 end
 
+
+# Nx size of the state
+# off_p contains the order for the off-diagonal component
+# off_rad contains the radius for the localization of the map
+# nonid_rad contains the number of non id components
+# dist matrix of the distances between the variables
+function setup_order(Nx, diagobs_p, diagunobs_p, off_p, off_rad, nonid_rad, dist::Array{Float64,2})
+
+	dist = convert(Array{Int64,2}, dist)
+    perm = sortperm(view(dist,:,1))
+
+    dist_to_order = fill(-1, Nx)
+    # fill!(view(dist_to_order,1:ceil(Int64,off_rad)), off_p)
+    fill!(view(dist_to_order,1:ceil(Int64,min(off_rad, Nx))), off_p)
+    order = [fill(-1,i) for i=1:Nx+1]
+    nonid = Int64[]
+    for i=1:Nx
+        node_i = perm[i]
+        dist_i1 = dist[1, node_i]
+
+        # Vector to store order, offset of 1
+        # because we have one scalar observation
+        order_i = zeros(Int64, i-1)#view(order,i+1)
+        if dist_i1 <= nonid_rad
+            if i>1
+            for j=1:i-1
+
+                # extract distance to node i
+                node_j = perm[j]
+                dist_ij = dist[node_i, node_j]
+
+                # compute order for variable based on distance
+                order_i[j] = dist_to_order[dist_ij]
+            end
+            end
+            # Define order for diagonal TM components and observation component
+            if i==1 # observed mode
+                order[2][2] = diagobs_p
+                order[2][1] = off_p
+            else # unobserved mode
+                order[i+1][2:i] .= deepcopy(order_i)
+                order[i+1][i+1] = diagunobs_p
+                order[i+1][1] = -1
+            end
+        end
+    end
+    return order
+end
 
 
 #
